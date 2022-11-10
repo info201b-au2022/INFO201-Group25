14b639116a3fb70cb4f7b6fb722d82acc258cbaa
bndn13 <bndn13@uw.edu>
2022-11-10 11:54:03 -0800

    added geographic data set

--- source/summary.R
+++ source/summary.R
@@ -1,3 +1,15 @@
 library(dplyr)
 demographics <- read.csv("https://raw.githubusercontent.com/info201b-au2022/INFO201-Group25/main/data/demographics.csv")
 View(demographics)
+
+# summary_info.R 
+# A source file that takes in a dataset and returns a list of info about it:
+summary_info <- list()
+summary_info$num_observations <- nrow(demographics)
+summary_info$race_max_deaths <- demographics %>%
+  filter(TotalDeaths == max(TotalDeaths, na.rm = T)) %>%
+  select(Ethnicity)
+summary_info$gender_max_deaths <- demographics %>%
+  filter(TotalDeaths == max(TotalDeaths, na.rm = T)) %>%
+  select(Gender)
+
