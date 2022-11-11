9110e0263982b323fa2fdc47f602f7a5152433ef
bndn13 <bndn13@uw.edu>
2022-11-10 19:42:26 -0800

    edited proposal

--- source/summary.R
+++ source/summary.R
@@ -1,6 +1,6 @@
 library(dplyr)
-demographics <- read.csv("https://raw.githubusercontent.com/info201b-au2022/INFO201-Group25/main/data/demographics.csv")
-View(demographics)
+geographics <- read.csv("https://raw.githubusercontent.com/info201b-au2022/INFO201-Group25/main/data/geographics.csv")
+View(geographics)
 
 # summary_info.R 
 # A source file that takes in a dataset and returns a list of info about it:
