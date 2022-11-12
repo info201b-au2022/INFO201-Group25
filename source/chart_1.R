1eb99960236632f447686eb2f96e6cef2868aece
wudel22 <delaneywu2004@hotmail.com>
2022-11-11 17:09:39 -0800

    finish chart 1

---
+++ source/chart_1.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+#Read in the geographics csv dataset
+geographics <- read.csv("https://raw.githubusercontent.com/info201b-au2022/INFO201-Group25/main/data/geographics.csv")
+View(geographics)
+
+#pull the deaths for each state into a table
+state_deaths <- geographics %>%
+  group_by(state) %>%
+  summarise(total_state_deaths = sum(n_killed))
+View(state_deaths)
+
+# Create histogram displaying the deaths in each state
+state_histogram <- state_deaths %>%
+  # filter(state == "Washington") %>%
+  ggplot(aes(x = state, y = total_state_deaths)) +
+  geom_bar(stat = "identity") +
+  ylim(0, 6000) +
+  ggtitle("State Chart")
+state_histogram
