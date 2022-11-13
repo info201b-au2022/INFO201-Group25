7c32cae9ac745de8a43a45b7491448f4aef917f3
bndn13 <bndn13@uw.edu>
2022-11-12 16:35:14 -0800

    changed summary

--- source/summary.R
+++ source/summary.R
@@ -1,15 +1,46 @@
 library(dplyr)
+library(tidyverse)
 geographics <- read.csv("https://raw.githubusercontent.com/info201b-au2022/INFO201-Group25/main/data/geographics.csv")
 View(geographics)
 
 # summary_info.R 
 # A source file that takes in a dataset and returns a list of info about it:
 
-# State
+# summary_info.R 
+# A source file that takes in a dataset and returns a list of info about it:
+summary_info <- list()
+summary_info$num_observations <- nrow(my_dataframe)
+summary_info$some_max_value <- my_dataframe %>%
+  filter(some_var == max(some_var, na.rm = T)) %>%
+  select(some_label)
 
-stateDF <- geographics %>%
-  group_by(state) %>%
-  summarize(stateDeaths = sum(n_killed))
-View(stateDF)
+
+# Total Deaths from 2013 - 2018
+totalDeaths <- geographics %>%
+  
+
+# State with Max Death
+
+
+# State with Least Death
+
+
+# City with Max Death
+
+
+# City with Least Death
+
+
+# Year with most Death
+
+
+# Year with least Death
+
+
+# Date with most Death
+
+
+# Date with least Death
+
 
 
