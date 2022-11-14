9df06eeb5ec9044a5e3faaacfdd72fd5b66ed55f
bndn13 <bndn13@uw.edu>
2022-11-13 17:29:00 -0800

    making changes to table.R

--- source/summary.R
+++ source/summary.R
@@ -3,20 +3,9 @@ library(tidyverse)
 geographics <- read.csv("https://raw.githubusercontent.com/info201b-au2022/INFO201-Group25/main/data/geographics.csv")
 View(geographics)
 
-# summary_info.R 
-# A source file that takes in a dataset and returns a list of info about it:
-
-# summary_info.R 
-# A source file that takes in a dataset and returns a list of info about it:
-summary_info <- list()
-summary_info$num_observations <- nrow(my_dataframe)
-summary_info$some_max_value <- my_dataframe %>%
-  filter(some_var == max(some_var, na.rm = T)) %>%
-  select(some_label)
-
 
 # Total Deaths from 2013 - 2018
-totalDeaths <- sum(geographics$n_killed)
+total_deaths <- sum(geographics$n_killed)
 print(totalDeaths)
 
 # State with total max death
