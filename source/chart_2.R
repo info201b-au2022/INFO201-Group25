86b5c976c87be7c58406dfc0fe67eec6634449ba
wudel22 <delaneywu2004@hotmail.com>
2022-11-15 16:25:40 -0800

    delete unnecessary files and rename chart 5 to chart 2

--- source/chart_2.R
+++ source/chart_2.R
@@ -1,61 +1,34 @@
-library(tidyverse)
+library(dplyr)
+library(leaflet)
 library(ggplot2)
-#install.packages("usmap")
-library(usmap)
 
-#Read in the geographics csv dataset
+# Read in the dataset
 geographics <- read.csv("https://raw.githubusercontent.com/info201b-au2022/INFO201-Group25/main/data/geographics.csv")
 View(geographics)
 
-#pull the deaths for each city/county into a table
-df <- data.frame("city" = geographics$city_or_county, "state" = geographics$state, "deaths" = geographics$n_killed, "lat" = geographics$latitude, "long" = geographics$longitude)  
-df <- unite(df, "location", city:state, sep = ", " , na.rm = TRUE, remove = FALSE)
-View(df)
+#Filter to get the specific state user wants to look at
+cities <- geographics %>%
+  filter(state == "Washington") %>% 
+  rename(lat = latitude) %>%
+  rename(long = longitude)
+View(cities)
 
-city <- df %>%
-  filter(state == "Washington") 
-View(city)
+# Add a radius column to get the radius of the points to be plotted
+city_most_deaths <- cities %>% 
+  mutate(radius = (n_killed/max(n_killed) * 3)^2)
 
-#create state chart
-city_death_by_state_chart <- 
-  plot_usmap(
-    include = c("WA"),
-    fill = "light blue") +
-  # geom_point(data = city,
-  #           aes(x = lat, y = long, size = deaths),
-  #           color = "gold") +
-  labs(title = "Gun Violence distribution by Cities in different States")
-city_death_by_state_chart
-
-
-map1 <- ggplot(city, aes( x = city , y = deaths, group=group)) + 
-  geom_polygon(aes(fill = df), color = "black")
-map1
-
-
-
-library(ggplot2)
-library(maps)
-#install.packages("mapdata")
-library(mapdata)
-
-usa <- map_data('usa')
-View(usa)
-
-
-
-#install.packages("plyr")
-library(plyr)
-usa_city <- rbind.fill(usa, city)
-View(usa_city)
-
-mapdata1 <- usa_city %>% filter(!is.na(usa_city$location))
-View(mapdata1)
-
-
-
-map_1 <- ggplot(usa_city, aes( x = long , y = lat, group=group)) + 
-  geom_polygon(aes(fill = cityDeaths), color = "black")
-map_1
-
+# Create map using leaflet that shows the deaths in the cities of specified state
+death_distribution_by_city_in_washington <- 
+  leaflet(city_most_deaths) %>% 
+  addTiles() %>%
+  addCircleMarkers(
+    lat = ~lat, 
+    lng = ~long,
+    popup = ~city_or_county, #the points are plotted on the city or county in the dataset
+    stroke = FALSE,
+    radius = ~radius, # based off the radius created previously by the number of deaths
+    fillOpacity = 0.5,
+    color = "Red"
+  )
+death_distribution_by_city_in_washington
 
