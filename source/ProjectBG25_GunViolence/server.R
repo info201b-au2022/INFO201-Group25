399038f433b9797f33e1267202c8c5654b048dde
bndn13 <bndn13@uw.edu>
2022-12-06 17:44:06 -0800

    blah

--- source/ProjectBG25_GunViolence/server.R
+++ source/ProjectBG25_GunViolence/server.R
@@ -8,19 +8,30 @@
 #
 
 library(shiny)
+library(dplyr)
+library(ggplot2)
 
 geographics <- read.csv("https://raw.githubusercontent.com/info201b-au2022/INFO201-Group25/main/data/geographics.csv")
+
+state_deaths <- geographics %>%
+  group_by(state) %>%
+  summarise(total_state_deaths = sum(n_killed)) 
 
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
   output$chart1 <- renderPlot({
+    
+    state_data <- state_deaths %>% 
+      filter(state %in% input$Country_1, state %in% input$Country_2 )
+    
     state_bar_chart <- ggplot(
-      data = state_deaths, 
+      data = state_data, 
       aes(x = state, 
           y = total_state_deaths)) +
       geom_col(fill = "navy blue") +
       ylim(0, 6000) + 
       ggtitle("State Death Chart ")
+    
     return (state_bar_chart)
   })
 
--- source/ProjectBG25_GunViolence/ui.R
+++ source/ProjectBG25_GunViolence/ui.R
@@ -27,14 +27,19 @@ Chart1 <- tabPanel(
   sidebarLayout(
     sidebarPanel(
       fluidRow(
-        selectInput(id = "Country_1", 
-                    label = h3("Select Country"), 
-                    choices = state_deaths$state, 
-                    selected = 1)
+        selectInput(inputId = "Country_1", 
+                    label = h3("Select Country"),
+                    choices = state_deaths$state,
+                    #choices = list("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"), 
+                    selected = "Alabama"), 
+        selectInput(inputId = "Country_2", 
+                     label = h3("Select Country"),
+                     choices = state_deaths$state,
+                     selected = "Alaska")
       )
     ),
     mainPanel(
-      #plotlyOutput("graph_line")
+      plotOutput("chart1")
     )
   )
 )
